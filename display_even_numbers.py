9ae7dba2a6f2273929d1a8f9d101aaf7dd085612
Deron Franklin Sobers II <deronfsobers2@gmail.com>
2020-12-20 19:44:04 -0600

    added more implementations

--- display_even_numbers.py
+++ display_even_numbers.py
@@ -4,6 +4,29 @@
 
 ##1 7:31PM on 12/20/2020
 for i in range(2,101,2):
-    print("this is #%d" % i)
+    print("Implementation #1: #%d" % i)
     i+=1
 
+print("\n\n")
+
+##2 
+i = 1
+while(i<= 100):
+    if(i%2 == 0):
+        print("Implementation #2: %d" % i)
+    i+=1
+
+print("\n\n")
+
+##3 Implementation #3 
+# Best ONE because least lines of code!
+for i in range(2,101,2):
+    print("Implementation #3: #%d" % i)
+
+print("\n\n")
+
+##4 Implementation #4 
+for i in range(1,101):
+    if(i % 2 == 0):
+        print("Implementation #4 %d" % i)
+    
